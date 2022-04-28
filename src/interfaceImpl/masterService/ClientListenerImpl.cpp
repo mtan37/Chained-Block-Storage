9e96690293b3f5c8572114f53c9d5f121d0f1c7e
ethane98 <eebrown9@wisc.edu>
2022-04-28 16:43:26 -0400

    Master GetConfig rpc

--- src/interfaceImpl/masterService/ClientListenerImpl.cpp
+++ src/interfaceImpl/masterService/ClientListenerImpl.cpp
@@ -8,19 +8,17 @@ grpc::Status master::ClientListenerImpl::GetConfig (
     grpc::ServerContext *context,
     const google::protobuf::Empty *request,
     master::GetConfigReply *reply) {
-//
-//        master::ServerIp head;
-//        master::Node headNode = master::nodeList.front();
-//        head->setIp(headNode.ip);
-//        head->setPort(headNode.port);
-//
-//        master::ServerIp tail;
-//        master::Node tailNode = master::nodeList.back();
-//        tail.setIp(tailNode.ip);
-//        tail.setPort(tailNode.port);
-//
-//        reply->setHead(head);
-//        reply->setTail(tail);
-//
-//        return grpc::Status::OK;
+
+        master::Node *headNode = master::head;
+        master::Node *tailNode = master::tail;
+
+        master::ServerIp * reply_head = reply->mutable_head();
+        reply_head->set_ip(headNode->ip);
+        reply_head->set_port(headNode->port);
+
+        master::ServerIp * reply_tail = reply->mutable_tail();
+        reply_tail->set_ip(tailNode->ip);
+        reply_tail->set_port(tailNode->port);
+
+        return grpc::Status::OK;
 }
