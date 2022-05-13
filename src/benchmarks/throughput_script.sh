d41560aaa780832753b28a6b663356b8b7713437
Marvin Tan <marvin.tan@wisc.edu>
2022-05-13 00:59:21 +0000

    throughput script modify

--- src/benchmarks/throughput_script.sh
+++ src/benchmarks/throughput_script.sh
@@ -3,8 +3,14 @@ counter=1
 while [ $counter -le 20 ]
 do
     echo $counter
-    #./throughput_benchmark 625 is_unaligned &
-    #./throughput_benchmark 625 &
+    ./throughput_benchmark 100 is_unaligned &
+    ./throughput_benchmark 100 &
+    
+    #./throughput_benchmark 100 &
+    #./throughput_benchmark 100 &
+    
+    #./throughput_benchmark 100 is_unaligned &
+    #./throughput_benchmark 100 is_unaligned &
     counter=$(( $counter + 1 ))
 done
 
